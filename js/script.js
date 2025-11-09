21b98f200eeb6333f79068c529100725f16576b9
CarolGP <cgpardal@gmail.com>
2025-11-09 21:33:00 +0100

    cambios en js para añadir títulos

--- js/script.js
+++ js/script.js
@@ -1,2 +1,37 @@
 import peliculas from './peliculas.js'
 
+/* Pelis Acción */
+const peliculasAccion = peliculas.filter(pelicula => pelicula.genre_ids.includes(28));
+console.log(peliculasAccion);
+
+const contenedorAccion = document.getElementById("genero-28");
+
+peliculasAccion.forEach(peli =>{
+    const p = document.createElement("p");
+    p.textContent = peli.title;
+    contenedorAccion.appendChild(p);
+})
+
+/* Pelis Thriller */
+const peliculasThriller = peliculas.filter(pelicula => pelicula.genre_ids.includes(53));
+console.log(peliculasThriller);
+
+const contenedorThriller = document.getElementById("genero-53");
+
+peliculasThriller.forEach(peli =>{
+    const p = document.createElement("p");
+    p.textContent = peli.title;
+    contenedorThriller.appendChild(p);
+})
+
+/* Pelis Aventura */
+const peliculasAventura = peliculas.filter(pelicula => pelicula.genre_ids.includes(12));
+console.log(peliculasAventura);
+
+const contenedorAventura = document.getElementById("genero-12");
+
+peliculasAventura.forEach(peli =>{
+    const p = document.createElement("p");
+    p.textContent = peli.title;
+    contenedorAventura.appendChild(p);
+})

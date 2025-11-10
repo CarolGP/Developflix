6f6c8e7095297b129444c9e49e52d025f5956fb2
CarolGP <cgpardal@gmail.com>
2025-11-10 20:41:29 +0100

    ejercicio finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import peliculas from './peliculas.js'
 
+/* const img = document.createElement("img");
+img.src = "https://image.tmdb.org/t/p/w500" + peli.poster_path; */
+
 /* Pelis Acción */
 const peliculasAccion = peliculas.filter(pelicula => pelicula.genre_ids.includes(28));
 console.log(peliculasAccion);
@@ -7,10 +10,16 @@ console.log(peliculasAccion);
 const contenedorAccion = document.getElementById("genero-28");
 
 peliculasAccion.forEach(peli =>{
+    const div= document.createElement("div");
+    const img= document.createElement("img");
+    img.src = "https://image.tmdb.org/t/p/w200" + peli.poster_path; img.alt = "poster peli";
     const p = document.createElement("p");
-    p.textContent = peli.title;
-    contenedorAccion.appendChild(p);
-})
+    p.textContent = peli.title + " " + peli.vote_average + " ⭐️";
+    div.appendChild(img);
+    div.appendChild(p);
+    contenedorAccion.appendChild(div);s
+});
+
 
 /* Pelis Thriller */
 const peliculasThriller = peliculas.filter(pelicula => pelicula.genre_ids.includes(53));
@@ -19,10 +28,17 @@ console.log(peliculasThriller);
 const contenedorThriller = document.getElementById("genero-53");
 
 peliculasThriller.forEach(peli =>{
+    const div = document.createElement("div");
+    const img= document.createElement("img");
+    img.src= "https://image.tmdb.org/t/p/w200" + peli.poster_path; img.alt = "poster peli";
     const p = document.createElement("p");
-    p.textContent = peli.title;
-    contenedorThriller.appendChild(p);
-})
+    p.textContent = peli.title + " " + peli.vote_average + " ⭐️";
+    div.appendChild(img);
+    div.appendChild(p);
+    contenedorThriller.appendChild(div);
+});
+
+
 
 /* Pelis Aventura */
 const peliculasAventura = peliculas.filter(pelicula => pelicula.genre_ids.includes(12));
@@ -31,7 +47,13 @@ console.log(peliculasAventura);
 const contenedorAventura = document.getElementById("genero-12");
 
 peliculasAventura.forEach(peli =>{
+    const div = document.createElement("div");
+    const img= document.createElement("img");
+    img.src= "https://image.tmdb.org/t/p/w200" + peli.poster_path; img.alt = "poster peli";
     const p = document.createElement("p");
-    p.textContent = peli.title;
-    contenedorAventura.appendChild(p);
-})
+    p.textContent = peli.title + " " + peli.vote_average + " ⭐️";
+    /* p.textContent = peli.vote_average; */
+    div.appendChild(img);
+    div.appendChild(p)
+    contenedorAventura.appendChild(div);
+});
